5a2078ac3818be4d7d4db50f9d39738856699c91
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-14 20:07:55 -0800

    first stab

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const App = () => <div>
+  <h1>Hello</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo veritatis neque voluptatem delectus, assumenda voluptate iste perspiciatis saepe ipsa alias eius quibusdam expedita minima, unde quas velit maiores aperiam laudantium.</p>
+</div>
+
+export default App
