1a8f8aae6223d3fb997254187ca721c6ef1412fe
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-15 17:25:51 -0800

    Moving App.tsx to components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const title: string = 'My Minimal React Webpack Babel Setup 39';
+const title: string = 'My Minimal React Webpack Babel Setup 2';
 
 const App = () => <div>{title}</div>
 
