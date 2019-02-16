f996aa98e08ceeae7e2b3dcf0f4ed9eab33437c1
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-15 17:26:03 -0800

    Simple test for scss

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      }, {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
       }
     ]
   },
