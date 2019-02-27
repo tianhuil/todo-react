e20937660e8dfc18c0a62148e4d7a908b35201ab
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-26 16:38:13 -0800

    Customizing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,93 +8,77 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
-const styles = (theme: Theme) => createStyles({
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+const styles = (theme: Theme) => {
+  const offWhite = fade(theme.palette.common.white, 0.8)
+
+  return createStyles({
+    root: {
+      flexGrow: 1,
     },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
+    logo: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'flex',
+      },
+      color: offWhite,
+      marginRight: theme.spacing.unit * 2,
     },
-    
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      width: '100%',
+      marginLeft: theme.spacing.unit * 2,
+      marginRight: theme.spacing.unit * 2,
+      [theme.breakpoints.up('sm')]: {
+        width: 300,
+      },
+      display: 'flex',
     },
-  },
-  growLeft: {
-    flexGrow: 1,
-    textAlign: "right",
-  }
-});
+    searchInput: {
+      marginLeft: 8,
+      flexGrow: 1,
+      color: 'inherit',
+      width: '100%',
+    },
+    searchIcon: {
+      flexGrow: 0,
+      padding: theme.spacing.unit,
+      color: 'inherit',
+    },
+    accountButton: {
+      marginLeft: theme.spacing.unit * 2,
+      flexGrow: 1,
+      textAlign: "right",
+      color: offWhite,
+    }
+  })
+};
 
 export interface Props extends WithStyles<typeof styles> {}
 
-function ButtonAppBar(props: Props) {
+function Header(props: Props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
+          <Typography variant="h6" className={classes.logo}>
             Todo List
           </Typography>
           <div className={classes.search}>
-            <div className={classes.searchIcon}>
+            <InputBase className={classes.searchInput} placeholder="Search&hellip;" />
+            <IconButton className={classes.searchIcon} aria-label="Search">
               <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search&hellip;"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
+            </IconButton>
           </div>
-          <div className={classes.growLeft}>
+          <div className={classes.accountButton}>
             <AccountCircle/>
           </div>
         </Toolbar>
@@ -103,8 +87,8 @@ function ButtonAppBar(props: Props) {
   );
 }
 
-ButtonAppBar.propTypes = {
+Header.propTypes = {
   classes: PropTypes.object.isRequired,
 } as any;
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(Header);
