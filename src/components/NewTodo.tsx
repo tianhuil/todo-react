195fd8419a74556ab0ccc64cb5aca039f1161033
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-27 00:01:14 -0800

    Fixing NewTodo layout

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = (theme: Theme) => createStyles({
   root: {
@@ -10,35 +11,47 @@ const styles = (theme: Theme) => createStyles({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    width: 400,
-    margin: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 1,
+    width: '100%',
+    maxWidth: 400,
+    [theme.breakpoints.up('sm')]: {
+      margin: theme.spacing.unit * 2,
+    },
   },
   input: {
-    marginLeft: 8,
+    marginLeft: theme.spacing.unit * 2,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 4,
+    },
     flex: 1,
   },
   icon: {
-    margin: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 1,
+    [theme.breakpoints.up('sm')]: {
+      margin: theme.spacing.unit * 2,
+    },
   },
 });
 
 export interface Props extends WithStyles<typeof styles> {}
 
-function NewTodo(props: Props) {
+function NewTodoComp(props: Props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
       <Input className={classes.input} placeholder="Add New Todo &hellip;" />
-      <Icon className={classes.icon} color="primary">
-        add_circle
-      </Icon>
+      <IconButton aria-label="Comments" className={classes.icon} >
+        <Icon color="primary">
+          add_circle
+        </Icon>
+      </IconButton>
     </div>
   );
 }
 
-NewTodo.propTypes = {
+NewTodoComp.propTypes = {
   classes: PropTypes.object.isRequired,
 } as any;
 
-export default withStyles(styles)(NewTodo);
+export default withStyles(styles)(NewTodoComp);
