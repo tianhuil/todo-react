8de996e7112b85782f41969ed0da04ea186fe53c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-28 16:23:52 -0800

    Add Todo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
 import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
+
+import { addTodo } from "../store/";
 
 const styles = (theme: Theme) => createStyles({
   root: {
@@ -33,25 +36,52 @@ const styles = (theme: Theme) => createStyles({
   },
 });
 
-export interface Props extends WithStyles<typeof styles> {}
+const mapDispatch = {
+  addTodo,
+}
 
-function AddTodoComp(props: Props) {
-  const { classes } = props;
+export interface Props extends WithStyles<typeof styles> {
+  addTodo: (text: string) => void
+}
 
-  return (
-    <div className={classes.root}>
-      <Input className={classes.input} placeholder="Add New Todo &hellip;" />
-      <IconButton aria-label="Comments" className={classes.icon} >
-        <Icon color="primary">
-          add_circle
-        </Icon>
-      </IconButton>
-    </div>
-  );
+class AddTodoComp extends React.Component<Props> {
+  input: React.RefObject<{}>
+
+  constructor(props: Props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
+
+  handleSubmit(event: FormEvent) {
+    this.props.addTodo(this.input.value)
+    this.input.value = ''
+    event.preventDefault()
+  }
+
+  render() {
+    const { classes, addTodo } = this.props;
+
+    return (
+      <form className={classes.root} onSubmit={this.handleSubmit}>
+        <Input className={classes.input} placeholder="Add New Todo &hellip;" inputRef={el => this.input=el}/>
+        <IconButton aria-label="Comments" className={classes.icon} onClick={this.handleSubmit} type="submit">
+          <Icon color="primary">
+            add_circle
+          </Icon>
+        </IconButton>
+      </form>
+    );
+  }
 }
 
 AddTodoComp.propTypes = {
   classes: PropTypes.object.isRequired,
 } as any;
 
-export default withStyles(styles)(AddTodoComp);
+export default connect(
+  null,
+  mapDispatch,
+)(
+  withStyles(styles)(AddTodoComp)
+)
