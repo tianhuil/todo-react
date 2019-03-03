2561a9559cb88dd8a58e3f932c0c929b990bc15d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-03 15:28:09 -0800

    Simplifying reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
 
 import { todoReducer } from './todos/reducers'
-import { Todo } from './todos/types'
-import { addTodo, toggleTodo, deleteTodo } from './todos/actions'
+import { addTodo, toggleTodo, deleteTodo, Todo } from './todos/actions'
 
 export const reducer = combineReducers({
   todo: todoReducer,
